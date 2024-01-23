aaed86f7d96f3f432880b8a5b57fbb8862d1c905
{daichi-nakatsuji} <{cgug1052@mail4.doshisha.ac.jp}>
2024-01-23 18:53:57 +0900

    完成版

--- tankgame/TankGame_client.c
+++ tankgame/TankGame_client.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <winsock2.h> //-lws2_32
-
+#include <time.h>
 
 int main(void) {	
 
@@ -15,9 +15,9 @@ int main(void) {
 	char destination[100];  //アドレス
 	int port;		//ポート番号
 
-	printf("アドレスを入力してください");
+	printf("adress=");
 	scanf("%s", destination);               //127.0.0.1
-	printf("ポート番号を入力してください");
+	printf("port=");
 	scanf("%d", &port);                     //11000
 	dest.sin_port = htons(port);
 	dest.sin_family = AF_INET;
@@ -37,7 +37,7 @@ int main(void) {
 	printf("%sに接続しました\n", destination);
 	
     char buffer[1024];
-	char name[] = "name:SAMPLE\n";
+	char name[] = "name:GoPT\n";
     char move[9][20] = {"move:100\n", "move:200\n", "move:300\n", "move:400\n", "move:500\n", "move:600\n", "move:700\n", "move:800\n", "move:900\n"};
 	char cannon[9][20] = {"cannon:100\n", "cannon:200\n", "cannon:300\n", "cannon:400\n", "cannon:500\n", "cannon:600\n", "cannon:700\n", "cannon:800\n", "cannon:900\n"};
 	char search[11][20] = {"search:0\n", "search:100\n", "search:200\n", "search:300\n", "search:400\n", "search:500\n", "search:600\n", "search:700\n", "search:800\n", "search:900\n", "search:1000\n"};
@@ -57,12 +57,13 @@ int main(void) {
 	printf("→ %s\n\n", buffer);
 
 	int Hight = 5;
-	int MyHight = 2;
+	int MyHight = 0;
 	int x, y;
 	int cannon_num;
 	char Search_Result[20];
 	char null[1024] = "search_result:null\n";
-
+	time_t t = time(NULL);
+	srand(t);
 
 	while(1){
 		//自身の座標を確認
@@ -70,10 +71,11 @@ int main(void) {
 		//サーバからデータを受信
         memset(buffer, '\0', sizeof(buffer));
 	    recv(s, buffer, sizeof(buffer), 0);
-	    printf("→ %s\n", buffer);
+	    printf("→ %s", buffer);
+		
 		//フィールドの上部と下部どちらにいるか
-		sscanf(buffer, "state_hight_result:[%d]", Hight);
-		Hight = Hight / 100;
+		sscanf(buffer, "state_hight_result:%d\n", &Hight);
+		Hight /= 100;
 		if(Hight >= 5){
 			MyHight = 0;
 		}else{
@@ -81,73 +83,113 @@ int main(void) {
 		}
 
 		//防御
-		switch (MyHight){
-		case 0:		//自身の座標が500以上の時
+		if(MyHight == 0){
 			//自身の上部を確認
 			send(s, search[0], strlen(search[0]), 0);
 			//サーバからデータを受信
         	memset(buffer, '\0', sizeof(buffer));
 	    	recv(s, buffer, sizeof(buffer), 0);
-	    	printf("→ %s\n", buffer);
+	    	printf("→ %s", buffer);
 
 			//移動指示
 			if(strcmp(buffer, null) == 0){
 				send(s, move[Hight - 5], strlen(move[Hight - 5]), 0);
+				//サーバからデータを受信
+    			memset(buffer, '\0', sizeof(buffer));
+	    		recv(s, buffer, sizeof(buffer), 0);
+	    		printf("→ %s", buffer);
 			}else{
-				sscanf(buffer, "search_result:[%d],[%d],[%s]\n", x, y, Search_Result);
+				sscanf(buffer, "search_result:%d,%d,%s\n", &x, &y, Search_Result);
 				if(y <= 700){
 					send(s, move[y/100 + 2], strlen(move[y/100 + 2]), 0);
+					//サーバからデータを受信
+    				memset(buffer, '\0', sizeof(buffer));
+	    			recv(s, buffer, sizeof(buffer), 0);
+	    			printf("→ %s", buffer);
 				}
 			}
-			//サーバからデータを受信
-    		memset(buffer, '\0', sizeof(buffer));
-	    	recv(s, buffer, sizeof(buffer), 0);
-	    	printf("→ %s\n", buffer);
-			break;
-		case 1:		//自身の座標が500未満の時
+		}else if(MyHight == 1){
 			//自身の下部を確認
 			send(s, search[10], strlen(search[10]), 0);
 			//サーバからデータを受信
         	memset(buffer, '\0', sizeof(buffer));
 	    	recv(s, buffer, sizeof(buffer), 0);
-	    	printf("→ %s\n", buffer);
+	    	printf("→ %s", buffer);
 			if(strcmp(buffer, null) == 0){
 				send(s, move[Hight + 4], strlen(move[Hight + 4]), 0);
+				//サーバからデータを受信
+    			memset(buffer, '\0', sizeof(buffer));
+	    		recv(s, buffer, sizeof(buffer), 0);
+	    		printf("→ %s", buffer);
 			}else{
-				sscanf(buffer, "search_result:[%d],[%d],[%s]\n", x, y, Search_Result);
+				sscanf(buffer, "search_result:%d,%d,%s\n", &x, &y, Search_Result);
 				if(y >= 200){
 					send(s, move[y/100 - 2], strlen(move[y/100 - 2]), 0);
+					//サーバからデータを受信
+    				memset(buffer, '\0', sizeof(buffer));
+	    			recv(s, buffer, sizeof(buffer), 0);
+	    			printf("→ %s", buffer);
 				}
 			}
-			//サーバからデータを受信
-    		memset(buffer, '\0', sizeof(buffer));
-	    	recv(s, buffer, sizeof(buffer), 0);
-	    	printf("→ %s\n", buffer);
-			break;
-		default:
-			break;
 		}
+
+		Sleep(1000*(rand()%3+1));
+		
 
 		//攻撃
 		send(s, state[2], strlen(state[2]), 0);
 		//サーバからデータを受信
     	memset(buffer, '\0', sizeof(buffer));
 		recv(s, buffer, sizeof(buffer), 0);
-    	printf("→ %s\n", buffer);
-		sscanf(buffer, "state_cannon_result:[%d]", cannon_num);
+    	printf("→ %s", buffer);
+		sscanf(buffer, "state_cannon_result:%d", &cannon_num);
 		//自身の高さから上下2か所にキャノンを打つ
-		if(cannon_num >= 2 && Hight > 2 && Hight < 9){
+		if(cannon_num >= 2 && Hight > 1 && Hight < 8){
 			send(s, cannon[Hight+1], strlen(cannon[Hight+1]), 0);
 			//サーバからデータを受信
     		memset(buffer, '\0', sizeof(buffer));
 	    	recv(s, buffer, sizeof(buffer), 0);
-	    	printf("→ %s\n", buffer);
+	    	printf("→ %s", buffer);
+			Sleep(1000);
+
+			send(s, cannon[Hight], strlen(cannon[Hight]), 0);
+			//サーバからデータを受信
+    		memset(buffer, '\0', sizeof(buffer));
+	    	recv(s, buffer, sizeof(buffer), 0);
+	    	printf("→ %s", buffer);
+			Sleep(1000);
 
 			send(s, cannon[Hight-1], strlen(cannon[Hight-1]), 0);
 			//サーバからデータを受信
     		memset(buffer, '\0', sizeof(buffer));
 	    	recv(s, buffer, sizeof(buffer), 0);
-	    	printf("→ %s\n", buffer);
+	    	printf("→ %s", buffer);
+		}else if(cannon_num >= 2 && Hight >= 8){
+			send(s, cannon[Hight - 2], strlen(cannon[Hight -1]), 0);
+			//サーバからデータを受信
+    		memset(buffer, '\0', sizeof(buffer));
+	    	recv(s, buffer, sizeof(buffer), 0);
+	    	printf("→ %s", buffer);
+			Sleep(1000);
+
+			send(s, cannon[Hight-1], strlen(cannon[Hight-1]), 0);
+			//サーバからデータを受信
+    		memset(buffer, '\0', sizeof(buffer));
+	    	recv(s, buffer, sizeof(buffer), 0);
+	    	printf("→ %s", buffer);
+		}else if(cannon_num >= 2 && Hight == 1){
+			send(s, cannon[Hight+1], strlen(cannon[Hight+1]), 0);
+			//サーバからデータを受信
+    		memset(buffer, '\0', sizeof(buffer));
+	    	recv(s, buffer, sizeof(buffer), 0);
+	    	printf("→ %s", buffer);
+			Sleep(1000);
+
+			send(s, cannon[Hight+2], strlen(cannon[Hight+2]), 0);
+			//サーバからデータを受信
+    		memset(buffer, '\0', sizeof(buffer));
+	    	recv(s, buffer, sizeof(buffer), 0);
+	    	printf("→ %s", buffer);
 		}
 	}
 
